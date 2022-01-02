671615f1cac39b85889a84ccb800df9f5515d71f
Anshuman <anshuman8swain@gmail.com>
2022-01-02 16:37:20 +0530

    Flight class made

--- Flight.java
+++ Flight.java
@@ -6,9 +6,94 @@ import java.util.*;
 
 
 public class Flight {
-    ArrayList<ArrayList> flight;
-    static int num;
-    Flight(ArrayList flights){
-        flight.add(flights);
+
+    private String froDesti;
+    private int num;
+    private String toDesti;
+    private String ftime;
+    private String fdate;
+    private int fprice;
+    static int count;
+
+    public Flight(int num,String froDesti, String toDesti, String ftime, String fdate, int price, int ind){
+        if (toDesti.equals(froDesti)) {
+            throw new IllegalArgumentException("destination and origin are the same");
+        }
+        this.num=num;
+        this.froDesti=froDesti;
+        this.toDesti=toDesti;
+        this.ftime=ftime;
+        this.fdate=fdate;
+        this.fprice=price;
+        count=ind;
     }
+    public String getFroDesti() {
+        return froDesti;
+    }
+
+    public void setFroDesti(String froDesti) {
+        this.froDesti = froDesti;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getToDesti() {
+        return toDesti;
+    }
+
+    public void setToDesti(String toDesti) {
+        this.toDesti = toDesti;
+    }
+
+    public String getFtime() {
+        return ftime;
+    }
+
+    public void setFtime(String ftime) {
+        this.ftime = ftime;
+    }
+
+    public String getFdate() {
+        return fdate;
+    }
+
+    public void setFdate(String fdate) {
+        this.fdate = fdate;
+    }
+
+    public int getFprice() {
+        return fprice;
+    }
+
+    public void setFprice(int fprice) {
+        this.fprice = fprice;
+    }
+
+    @Override
+    public String toString() {
+        String one = "Flight " + num;
+        String two = "," + froDesti + " to " + toDesti;
+        String three = "," + ftime;
+        String four = ", original price: " + fprice + "Rs.";
+        return one + two + three + four;
+    }
+
+    //    static ArrayList<ArrayList> flight;
+//    static int num;
+//
+//    public Flight(ArrayList flights){
+//        flight.add(flights);
+//    }
+//    public static int getNum(){
+//        return num;
+//    }
+//    public static ArrayList searchFli(){
+//
+//    }
 }
--- Main.java
+++ Main.java
@@ -2,59 +2,87 @@ package Flight;
 import java.util.*;
 
 public class Main {
-
+//--------------------------------------------------------------------------------------------------------------------------------admin
    protected static void adminMethod(){
+        Flight[] index=new Flight[100];
         Scanner sc= new Scanner(System.in);
         System.out.println(" Welcome Admin, What do you want to do next: ");
         System.out.println("[1 : Add flight ]");
         System.out.println("[2 : Remove flight]");
         System.out.println("[3 : Display all flights]");
         byte mainChoice=sc.nextByte();
+        //ArrayList flightRef=new ArrayList();
         //Byte mainComp=-1;
         if(mainChoice==1){
+//            char a=(char) Flight.getNum();
+            //ArrayList newFlight=new ArrayList();
             System.out.println("Scheduling a new flight\n ");
             System.out.print("Enter the flight number: ");
             int fli_num=sc.nextInt();
+//            newFlight.add(fli_num);
             sc.nextLine();
             System.out.print("Enter flight from City: ");
             String fli_from=sc.nextLine();
-            //sc.nextLine();
+//            newFlight.add(fli_from);
             System.out.print("Enter flight Destination City: ");
             String fli_to=sc.nextLine();
-            //sc.nextLine();
+//            newFlight.add(fli_to);
             System.out.print("Enter cost per ticket: ");
             int price=sc.nextInt();
+//            newFlight.add(price);
+            sc.nextLine();
             System.out.print("Enter flight date (dd-mm-yyyy): ");
             String fli_date=sc.nextLine();
-            sc.nextLine();
+//            newFlight.add(fli_date);
             System.out.print("Enter flight time: ");
             //aasc.nextLine();
             String fli_tim=sc.nextLine();
+            index[Flight.count]=new Flight(fli_num,fli_from,fli_to,fli_tim,fli_date,price,Flight.count+1);
+//            newFlight.add(fli_tim);
+            //System.out.print("Enter Flight Code(Should be unique): ");
+           // ArrayList flightRef[1]=new ArrayList();
+            System.out.println("\nNew flight added to Amrita Domestic\n");
+//            System.out.println(newFlight);
+//            flightMain.add(newFlight);
+            //Flight refVar= new Flight(newFlight);
 
         }
         else if(mainChoice==2){
 
         }
         else if(mainChoice==3){
-
+            int tot=0;
+            for (int i=0;i<100;i++){
+//              try {
+//                  System.out.println("\n " + index[i].getNum() + "" + index[i].getFroDesti() + "" + index[i].getToDesti() + "" + index[i].getFprice() + "" + index[i].getFdate() + "" + index[i].getFtime() + "");
+//                  tot++;
+//              }
+//              catch (Exception e){
+//                  Exception vcxfg=e;
+//              }
+                index[i].toString();
+            }
+            System.out.println("\ntotal "+tot+" displayed");
         }
         else{
             System.out.println(" Invalid input ");
         }
     }
-
-    static void passengerMethod(){
+// -----------------------------------------------------------------------------------------------------------------------------------passenger
+    protected static void passengerMethod(){
        
 
     }
+
+    //---------------------------------------------------------------------------------------------------------------------------------main
         // main method
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         byte ch=1;
 
-
         while(ch==1){
-            System.out.println("\n\nFlight Ticket Management System\n");
+            System.out.println("\n\n---***Amrita Airlines***---");
+            System.out.println("\nFlight Ticket Management System\n");
             System.out.println("-----------Welcome-----------\n");
             System.out.println("Are you a Passenger or Airline admin?");
             System.out.print("[Passenger: p] ; [Airline Admin: a], input: ");
@@ -82,8 +110,14 @@ public class Main {
             else{
                 System.out.println(" Invalid input ");
             }
-
-            ch=0;   // to break the main while loop
+            System.out.println("Do you want to continue? ");
+            System.out.print("[Y:yes]  [N: Exit] : ");
+            String che=sc.nextLine();
+            if (che.charAt(0)=='n'|| che.charAt(0)=='N'){
+                System.out.println("\nThank you");
+                ch=0;
+            }
+            //ch=0;   // to break the main while loop
         }
     }
 }
